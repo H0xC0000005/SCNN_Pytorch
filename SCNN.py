006d9d57f7122c12d10921344dafbbc400a09310
H0xC0000005 <74247664+H0xC0000005@users.noreply.github.com>
2022-11-30 22:33:48 +0800

    commented SCNN architecture

--- SCNN.py
+++ SCNN.py
@@ -16,28 +16,40 @@ class SCNN(nn.Module):
             ms_ks: kernel size in message passing conv
         """
         super(SCNN, self).__init__()
+        self.message_passing = None
+        self.conv_after_backbone = None
+        self.final_conv = None
+        self.final_pool = None
+        self.final_fc = None
+        self.fc_input_feature = None
+        self.backbone = None
         self.pretrained = pretrained
         self.net_init(input_size, ms_ks)
         if not pretrained:
             self.weight_init()
 
-        self.scale_background = 0.4
-        self.scale_seg = 1.0
-        self.scale_exist = 0.1
+        """
+        predefined hyperparams
+        """
+        self.scale_background = 0.4  # ?
+        self.scale_seg = 1.0  # ?
+        self.scale_exist = 0.1  # ?
 
+        # predefined loss (cross entropy)
         self.ce_loss = nn.CrossEntropyLoss(weight=torch.tensor([self.scale_background, 1, 1, 1, 1]))
         self.bce_loss = nn.BCELoss()
 
     def forward(self, img, seg_gt=None, exist_gt=None):
+        # CNN backbone as SCNN can just be the last layer of a feature mapping
         x = self.backbone(img)
-        x = self.layer1(x)
+        x = self.conv_after_backbone(x)
         x = self.message_passing_forward(x)
-        x = self.layer2(x)
+        x = self.final_conv(x)
 
         seg_pred = F.interpolate(x, scale_factor=8, mode='bilinear', align_corners=True)
-        x = self.layer3(x)
+        x = self.final_pool(x)
         x = x.view(-1, self.fc_input_feature)
-        exist_pred = self.fc(x)
+        exist_pred = self.final_fc(x)
 
         if seg_gt is not None and exist_gt is not None:
             loss_seg = self.ce_loss(seg_pred, seg_gt)
@@ -51,6 +63,9 @@ class SCNN(nn.Module):
         return seg_pred, exist_pred, loss_seg, loss_exist, loss
 
     def message_passing_forward(self, x):
+        """
+        convolve through 4 dimensions
+        """
         Vertical = [True, True, False, False]
         Reverse = [False, True, False, True]
         for ms_conv, v, r in zip(self.message_passing, Vertical, Reverse):
@@ -59,48 +74,62 @@ class SCNN(nn.Module):
 
     def message_passing_once(self, x, conv, vertical=True, reverse=False):
         """
+        actually this is spacial convolution over a simple direction
+
         Argument:
         ----------
         x: input tensor
         vertical: vertical message passing or horizontal
         reverse: False for up-down or left-right, True for down-up or right-left
+
+        conv: a bit magic, this is applied to each output slice and size keep unchanged, so
+        it implicitly requires padding
         """
-        nB, C, H, W = x.shape
+        nB, C, H, W = x.shape  # [batch channel height width], for a result of convolutions
         if vertical:
-            slices = [x[:, :, i:(i + 1), :] for i in range(H)]
-            dim = 2
+            slices = [x[:, :, i:(i + 1), :] for i in range(H)]  # horizontal slices for a single sample
+            dim = 2  # slice on height ie. get horizontal slices
         else:
-            slices = [x[:, :, :, i:(i + 1)] for i in range(W)]
+            slices = [x[:, :, :, i:(i + 1)] for i in range(W)]  # same, verical slices
             dim = 3
         if reverse:
             slices = slices[::-1]
+        # at this point slices is in specified order
 
         out = [slices[0]]
         for i in range(1, len(slices)):
+            # convolution @ add through dimension
             out.append(slices[i] + F.relu(conv(out[i - 1])))
         if reverse:
+            # flip back to original order if reversed previously
             out = out[::-1]
-        return torch.cat(out, dim=dim)
+        return torch.cat(out, dim=dim)  # return to original format
 
     def net_init(self, input_size, ms_ks):
         input_w, input_h = input_size
-        self.fc_input_feature = 5 * int(input_w/16) * int(input_h/16)
+        self.fc_input_feature = 5 * int(input_w / 16) * int(input_h / 16)
         self.backbone = models.vgg16_bn(pretrained=self.pretrained).features
 
         # ----------------- process backbone -----------------
         for i in [34, 37, 40]:
+            # magic numbers to get padding, not sure what these layers are for
+            # but these lines are just copying conv weights, not sure about their purpose
             conv = self.backbone._modules[str(i)]
+            # dilated conv need more padding
             dilated_conv = nn.Conv2d(
                 conv.in_channels, conv.out_channels, conv.kernel_size, stride=conv.stride,
                 padding=tuple(p * 2 for p in conv.padding), dilation=2, bias=(conv.bias is not None)
             )
             dilated_conv.load_state_dict(conv.state_dict())
             self.backbone._modules[str(i)] = dilated_conv
+        # also not sure why pop these modules, may be dropouts accd to original paper
         self.backbone._modules.pop('33')
         self.backbone._modules.pop('43')
 
         # ----------------- SCNN part -----------------
-        self.layer1 = nn.Sequential(
+
+        # cap 2 final conv layers to backbone features
+        self.conv_after_backbone = nn.Sequential(
             nn.Conv2d(512, 1024, 3, padding=4, dilation=4, bias=False),
             nn.BatchNorm2d(1024),
             nn.ReLU(),
@@ -110,6 +139,8 @@ class SCNN(nn.Module):
         )
 
         # ----------------- add message passing -----------------
+
+        # these conv are passed into self.message_passing_once() to process each slice of a sample
         self.message_passing = nn.ModuleList()
         self.message_passing.add_module('up_down', nn.Conv2d(128, 128, (1, ms_ks), padding=(0, ms_ks // 2), bias=False))
         self.message_passing.add_module('down_up', nn.Conv2d(128, 128, (1, ms_ks), padding=(0, ms_ks // 2), bias=False))
@@ -117,19 +148,19 @@ class SCNN(nn.Module):
                                         nn.Conv2d(128, 128, (ms_ks, 1), padding=(ms_ks // 2, 0), bias=False))
         self.message_passing.add_module('right_left',
                                         nn.Conv2d(128, 128, (ms_ks, 1), padding=(ms_ks // 2, 0), bias=False))
-        # (nB, 128, 36, 100)
+        # (nB, 128, 36, 100) as message passing does not change a single sample size
 
         # ----------------- SCNN part -----------------
-        self.layer2 = nn.Sequential(
+        self.final_conv = nn.Sequential(
             nn.Dropout2d(0.1),
             nn.Conv2d(128, 5, 1)  # get (nB, 5, 36, 100)
         )
 
-        self.layer3 = nn.Sequential(
+        self.final_pool = nn.Sequential(
             nn.Softmax(dim=1),  # (nB, 5, 36, 100)
             nn.AvgPool2d(2, 2),  # (nB, 5, 18, 50)
         )
-        self.fc = nn.Sequential(
+        self.final_fc = nn.Sequential(
             nn.Linear(self.fc_input_feature, 128),
             nn.ReLU(),
             nn.Linear(128, 4),
